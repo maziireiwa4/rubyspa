266a304db3bf058f441e97cbc269e326a0b56af7
maziireiwa4 <maziireiwa4@gmail.com>
2025-03-02 10:58:19 +0900

    Cập nhật mới

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection } from "firebase/firestore";
 
+// Lấy config từ biến môi trường
 const firebaseConfig = {
-  apiKey: "AIzaSyC8J20vzi4v-qBIzJV_UY3iXd_WdQUPuys",
-  authDomain: "rubyspa-99446.firebaseapp.com",
-  projectId: "rubyspa-99446",
-  storageBucket: "rubyspa-99446.firebasestorage.app",
-  messagingSenderId: "810195616823",
-  appId: "1:810195616823:web:26398ce010eed0f98babb2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
+// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const productsCollection = collection(db, "products");
-
