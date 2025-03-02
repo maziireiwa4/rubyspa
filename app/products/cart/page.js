45c245666d271377e20219e26a2e8ed07d630c4b
maziireiwa4 <maziireiwa4@gmail.com>
2025-03-02 12:55:46 +0900

    Cập nhật mới

--- app/products/cart/page.js
+++ app/products/cart/page.js
@@ -2,7 +2,7 @@ export default function CartPage() {
   return (
     <div>
       <h1>Giỏ Hàng</h1>
-      <p>Danh sách sản phẩm đã thêm vào giỏ hàng.</p>
+      <p>Đang phát triển.</p>
     </div>
   );
 }
